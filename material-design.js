63a16390737de9c29fc27379c1117a4956f026e1
JacobTM <JacobTM@users.noreply.github.com>
2017-09-07 14:44:38 -0400

    Add files via upload

---
+++ material-design.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};
+
+const tilesData = [
+  {
+    img: 'images/tech1-sublimetext.jpg',
+    title: 'event1',
+    author: 'bereainnovative',
+  },
+  {
+    img: 'images/tech2-github.jpg',
+    title: 'event2',
+    author: 'bereainnovative',
+  },
+  {
+    img: 'images/sideprojectsberea.jpg',
+    title: 'side-projects',
+    author: 'bereainnovative',
+  },
+  {
+    img: 'images/react.png',
+    title: 'event3',
+    author: 'bereainnovative',
+  },
+];
+
+/**
+ * A simple example of a scrollable `GridList` containing a [Subheader](/#/components/subheader).
+ */
+const GridListExampleSimple = () => (
+  <div style={styles.root}>
+    <GridList
+      cellHeight={180}
+      style={styles.gridList}
+    >
+      <Subheader>December</Subheader>
+      {tilesData.map((tile) => (
+        <GridTile
+          key={tile.img}
+          title={tile.title}
+          subtitle={<span>by <b>{tile.author}</b></span>}
+          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+        >
+          <img src={tile.img} />
+        </GridTile>
+      ))}
+    </GridList>
+  </div>
+);
+
+export default GridListExampleSimple;
